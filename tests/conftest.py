a461ec6d1f54518cca1a2154027a6f11b9eea693
Your Name <jackm.medda@gmail.com>
2025-01-13 12:40:07 +0100

    Cmd testing order fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 def pytest_collection_modifyitems(session, config, items):
+    cmd_test_name = "test_config_command_line"
     items_names = [item.name for item in items]
-    cmd_line_test_index = items_names.index("test_config_command_line")
-    items.insert(len(items), items.pop(cmd_line_test_index))
+    if cmd_test_name in items_names:
+        cmd_line_test_index = items_names.index(cmd_test_name)
+        items.insert(len(items), items.pop(cmd_line_test_index))
