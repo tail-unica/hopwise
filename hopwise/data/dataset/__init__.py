516669286a91f454b92c66ab917cf5b5760064f4
Your Name <jackm.medda@gmail.com>
2025-03-31 09:34:54 +0200

    Merge from main and improved inheritance user kg_path_dataset

--- hopwise/data/dataset/__init__.py
+++ hopwise/data/dataset/__init__.py
@@ -3,5 +3,5 @@ from hopwise.data.dataset.sequential_dataset import SequentialDataset
 from hopwise.data.dataset.kg_dataset import KnowledgeBasedDataset, UserItemKnowledgeBasedDataset
 from hopwise.data.dataset.kg_seq_dataset import KGSeqDataset
 from hopwise.data.dataset.decisiontree_dataset import DecisionTreeDataset
-from hopwise.data.dataset.kg_path_dataset import KnowledgePathDataset
+from hopwise.data.dataset.kg_path_dataset import KnowledgePathDataset, UserItemKnowledgePathDataset
 from hopwise.data.dataset.customized_dataset import *
