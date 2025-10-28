9dde56aaca0591db716679a0ba925a149f2961ca
jackmedda <jackm.medda@gmail.com>
2025-10-28 17:52:43 +0100

    Fix cache reset. Train fine

--- hopwise/model/knowledge_aware_recommender/kgrec.py
+++ hopwise/model/knowledge_aware_recommender/kgrec.py
@@ -344,6 +344,8 @@ class KGRec(KnowledgeRecommender):
         Returns:
             torch.Tensor: Training loss, shape: []
         """
+        if self.restore_user_e is not None or self.restore_entity_e is not None:
+            self.restore_user_e, self.restore_entity_e = None, None
 
         user = interaction[self.USER_ID]
         pos_item = interaction[self.ITEM_ID]
@@ -367,43 +369,6 @@ class KGRec(KnowledgeRecommender):
 
         total_loss = bpr_loss + mae_loss + cl_loss
         return total_loss
-
-    def predict(self, interaction):
-        user = interaction[self.USER_ID]
-        item = interaction[self.ITEM_ID]
-
-        user_all_embeddings, entity_all_embeddings = self.gcn(
-            self.user_embedding.weight,
-            self.entity_embedding.weight,
-            self.edge_index,
-            self.edge_type,
-            self.inter_edge._indices(),
-            self.inter_edge._values(),
-        )
-
-        u_embeddings = user_all_embeddings[user]
-        i_embeddings = entity_all_embeddings[item]
-        scores = torch.mul(u_embeddings, i_embeddings).sum(dim=1)
-        return scores
-
-    def full_sort_predict(self, interaction):
-        user = interaction[self.USER_ID]
-        if self.restore_user_e is None or self.restore_entity_e is None:
-            self.restore_user_e, self.restore_entity_e = self.gcn(
-                self.user_embedding.weight,
-                self.entity_embedding.weight,
-                self.edge_index,
-                self.edge_type,
-                self.inter_edge._indices(),
-                self.inter_edge._values(),
-            )
-
-        u_embeddings = self.restore_user_e[user]
-        i_embeddings = self.restore_entity_e[: self.n_items]
-
-        scores = torch.matmul(u_embeddings, i_embeddings.transpose(0, 1))
-
-        return scores.view(-1)
 
     def relation_aware_edge_sampling(self, sampling_rate=0.5):
         # exclude interaction
@@ -493,3 +458,40 @@ class KGRec(KnowledgeRecommender):
         # scores = (pos1 - head_embs).sum(dim=1).abs().mean(dim=0)
         scores = -torch.log(torch.sigmoid(torch.mul(pos1, head_embs).sum(1))).mean()
         return scores
+    
+    def predict(self, interaction):
+        user = interaction[self.USER_ID]
+        item = interaction[self.ITEM_ID]
+
+        user_all_embeddings, entity_all_embeddings = self.gcn(
+            self.user_embedding.weight,
+            self.entity_embedding.weight,
+            self.edge_index,
+            self.edge_type,
+            self.inter_edge._indices(),
+            self.inter_edge._values(),
+        )
+
+        u_embeddings = user_all_embeddings[user]
+        i_embeddings = entity_all_embeddings[item]
+        scores = torch.mul(u_embeddings, i_embeddings).sum(dim=1)
+        return scores
+
+    def full_sort_predict(self, interaction):
+        user = interaction[self.USER_ID]
+        if self.restore_user_e is None or self.restore_entity_e is None:
+            self.restore_user_e, self.restore_entity_e = self.gcn(
+                self.user_embedding.weight,
+                self.entity_embedding.weight,
+                self.edge_index,
+                self.edge_type,
+                self.inter_edge._indices(),
+                self.inter_edge._values(),
+            )
+
+        u_embeddings = self.restore_user_e[user]
+        i_embeddings = self.restore_entity_e[: self.n_items]
+
+        scores = torch.matmul(u_embeddings, i_embeddings.transpose(0, 1))
+
+        return scores.view(-1)
