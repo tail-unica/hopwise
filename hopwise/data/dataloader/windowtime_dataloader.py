b48bc01b07a320eecdf90a4a77ee0b91126bb227
Your Name <jackm.medda@gmail.com>
2026-01-29 12:17:15 +0100

    Draft implementation of windowed time dataloader

--- hopwise/data/dataloader/windowtime_dataloader.py
+++ hopwise/data/dataloader/windowtime_dataloader.py
@@ -6,7 +6,8 @@
 ################################################
 """
 
-import itertools
+# ruff: noqa: PLW0602 PLW0603
+
 from collections.abc import Iterator
 
 import torch
@@ -19,71 +20,71 @@ start_iter = False
 
 class WindowedTimeSampler(torch.utils.data.SequentialSampler):
     """A sampler that samples indices based on a fixed time window and yields them batched.
+    It is assumed that the data source is sorted by timestamp in ascending order.
 
     Args:
         data_source (Dataset): The dataset to sample from.
         window_size (int): The size of the time window.
 
     Example:
-        >>> data_source_format = ["user_id", "item_id", "timestamp"]
-        >>> data_source = [[0, 1, 2], [0, 2, 3], [1, 2, 4], [1, 3, 5], [0, 3, 6]]
+        >>> data_source_format = ["timestamp"]
+        >>> data_source = [2, 3, 4, 5, 6]
         >>> list(WindowedTimeSampler(data_source=data_source, window_size=3))
-        >>> [[0, 1, 2], [0, 2, 3], [1, 2, 4]], [[1, 3, 5], [0, 3, 6]]
+        >>> [[0, 1, 2], [3, 4]]
         >>> list(WindowedTimeSampler(data_source=data_source, window_size=2))
-        >>> [[0, 1, 2], [0, 2, 3]], [[1, 2, 4], [1, 3, 5]], [[0, 3, 6]]
+        >>> [[0, 1], [2, 3]], [[4]]
     """
 
     def __init__(self, data_source, window_size):
+        if not isinstance(window_size, int) and not isinstance(window_size, float) or window_size <= 0:
+            raise ValueError(f"window_size should be a positive number, but got window_size={window_size}")
+
         self.data_source = data_source
-        self.window_size = window_size
-        self.indices = self._generate_indices()
-
-    def _generate_indices(self):
-        pass
-
-    # def __init__(
-    #     self,
-    #     sampler: Union[Sampler[int], Iterable[int]],
-    #     batch_size: int,
-    #     drop_last: bool,
-    # ) -> None:
-    #     # Since collections.abc.Iterable does not check for `__getitem__`, which
-    #     # is one way for an object to be an iterable, we don't do an `isinstance`
-    #     # check here.
-    #     if not isinstance(batch_size, int) or isinstance(batch_size, bool) or batch_size <= 0:
-    #         raise ValueError(f"batch_size should be a positive integer value, but got batch_size={batch_size}")
-    #     if not isinstance(drop_last, bool):
-    #         raise ValueError(f"drop_last should be a boolean value, but got drop_last={drop_last}")
-    #     self.sampler = sampler
-    #     self.batch_size = batch_size
-    #     self.drop_last = drop_last
+        self.window_size = float(window_size)
 
     def __iter__(self) -> Iterator[list[int]]:
-        # Implemented based on the benchmarking in https://github.com/pytorch/pytorch/pull/76951
-        sampler_iter = iter(self.sampler)
-        if self.drop_last:
-            # Create multiple references to the same iterator
-            args = [sampler_iter] * self.batch_size
-            for batch_droplast in zip(*args):
-                yield [*batch_droplast]
-        else:
-            batch = [*itertools.islice(sampler_iter, self.batch_size)]
-            while batch:
-                yield batch
-                batch = [*itertools.islice(sampler_iter, self.batch_size)]
+        source_iter = iter(self.data_source)
+
+        class window_takewhile:
+            def __init__(self, source_size):
+                self._last = None
+                self.iter_indexer = iter(range(source_size))
+
+            def __call__(self, predicate, iterable):
+                for i, x in zip(self.iter_indexer, iterable):
+                    if predicate(x):
+                        yield i
+                    else:
+                        self._last = i
+                        return
+
+            @property
+            def last(self):
+                if self._last is None:
+                    raise AttributeError("No last element stored.")
+                ret = self._last
+                self._last = None
+                return ret
+
+        _takewhile = window_takewhile(len(self.data_source))
+
+        start_window = next(source_iter)
+        batch = [start_window, *_takewhile(lambda x: x - start_window < self.window_size, source_iter)]
+        while batch:
+            yield batch
+            try:
+                start_window = _takewhile.last
+                batch = [start_window, *_takewhile(lambda x: x - start_window < self.window_size, source_iter)]
+            except StopIteration:
+                batch = []
 
     def __len__(self) -> int:
-        # Can only be called if self.sampler has __len__ implemented
-        # We cannot enforce this condition, so we turn off typechecking for the
-        # implementation below.
-        # Somewhat related: see NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
-        if self.drop_last:
-            return len(self.sampler) // self.batch_size  # type: ignore[arg-type]
-        else:
-            return (len(self.sampler) + self.batch_size - 1) // self.batch_size  # type: ignore[arg-type]
+        # This is an approximation since we cannot know the exact number of windows
+        # without iterating through the entire data source.
+        return (len(self.data_source) + self.window_size - 1) // self.window_size
 
 
-class DistributedWindowedTimeSampler(torch.utils.data.Sampler):
+class DistributedWindowedTimeSampler(torch.utils.data.distributed.DistributedSampler):
     def __init__(self):
         raise NotImplementedError("Distributed sampling is not implemented for WindowedTimeSampler yet.")
 
@@ -103,7 +104,6 @@ class WindowedTimeDataLoader(torch.utils.data.DataLoader):
         self._dataset = dataset
         self._sampler = sampler
         self.sample_size = len(dataset)
-        self._init_batch_size_and_step()
         index_sampler = WindowedTimeSampler(list(range(self.sample_size)), window_size=self.config["time_window_size"])
 
         self.transform = construct_transform(config)
@@ -132,21 +132,22 @@ class WindowedTimeDataLoader(torch.utils.data.DataLoader):
             config (Config): The new config of dataloader.
         """
         self.config = config
-        self._init_batch_size_and_step()
 
-    def collate_fn(self):
-        """Collect the sampled index, and apply neg_sampling or other methods to get the final data."""
-        raise NotImplementedError("Method [collate_fn] must be implemented.")
+    def collate_fn(self, index):
+        index = torch.tensor(index)
+        data = self._dataset[index]
+        transformed_data = self.transform(self._dataset, data)
+        return transformed_data  # self._neg_sampling(transformed_data)
 
     def __iter__(self):
-        # global start_iter
-        # start_iter = True
+        global start_iter
+        start_iter = True
         res = super().__iter__()
-        # start_iter = False
+        start_iter = False
         return res
 
     def __getattribute__(self, __name: str):
-        # global start_iter
+        global start_iter
         if not start_iter and __name == "dataset":
             __name = "_dataset"
         return super().__getattribute__(__name)
